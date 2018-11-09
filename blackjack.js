b1333c20e778ee6c70fb2740ec85f8e4f33422a8
Samantha Helene Thomas <2shthomas@gmail.com>
2018-11-09 08:27:08 -0700

    first commit

---
+++ blackjack.js
@@ -0,0 +1,28 @@
+
+
+
+let card1 = "Ace of Spades";
+let card2 = 'Ten of Hearts';
+
+console.log('Welcome to Blackjack');
+
+console.log('You are dealt')
+
+console.log(" "+card1);
+console.log("  "+ card2);
+
+
+//Variables store variables
+//you declare a variable with the let 
+//
+
+
+
+
+//values
+//boolean
+//array
+//undefined and null
+            //if you want to blank out a variable, you set it to null
+            
+//strings and numbers
